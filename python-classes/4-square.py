0032902660e45676402637bbfa186782191b1a12
Kaan Kartal Kuyucu <kkaankartal@gmail.com>
2026-05-05 09:30:06 +0000

    Defines a Square area

---
+++ python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Defines a Square class with size property and validation."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        """Initializes the square with size validation."""
+        self.size = size  # setter'ı tetikler
+
+    @property
+    def size(self):
+        """Retrieves the size."""
+        return self.__size
+ize.setter
+    def size(self, value):
+        """Sets the size with validatio       if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the current square area."""
+        return self.__size * self.__size
