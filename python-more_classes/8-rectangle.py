093d8d6c3b8d29023648da380cb606b2fe14e65f
Kaan Kartal Kuyucu <kkaankartal@gmail.com>
2026-05-06 07:25:05 +0000

    8-rec

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -70,7 +70,7 @@ class Rectangle:
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
 
-    @staticmetho
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """Returns the rectangle with the bigger area."""
         if not isinstance(rect_1, Rectangle):
