3832806b4508818d14ab93fbe33df3a73371f83e
Kaan Kartal Kuyucu <kkaankartal@gmail.com>
2026-05-06 15:26:54 +0000

    readfile

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -7,4 +7,4 @@ This module cntains a function that writes a sting to a text file.
 def write_file(filename="", text=""):
     """a UTF-8 text file and return number of charcters."""
     with open(filename, "w", encoding="utf-8") as f:
-        retrn f.write(text)
+        return f.write(text)
