6ca0d27dacd6efa95dca92d623611cded946b9e1
Kaan Kartal Kuyucu <kkaankartal@gmail.com>
2026-05-06 15:14:44 +0000

    readfile

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+This module contains a function that reads a text file and prints its content.
+"""
+
 def read_file(filename=""):
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
