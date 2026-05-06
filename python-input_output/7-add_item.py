1b1a8f077e47edaa3f95d50e41d8983df8c051cd
Kaan Kartal Kuyucu <kkaankartal@gmail.com>
2026-05-06 16:55:28 +0000

    7-add

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Script that adds command line arguments to a list
+and saves them to add_item.json.
+"""
+
+import sys
+from 5-save_to_json_file import save_to_json_file
+from 6-load_from_json_file import load_from_json_file
+
+filename = "add_item.json"
