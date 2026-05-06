6756313810ce7a373fb0d06fb050beba02402591
Kaan Kartal Kuyucu <kkaankartal@gmail.com>
2026-05-06 16:07:58 +0000

    5-save_to_json

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module contains a function that converts a JSON string to a Python object.
+"""
+
+import json
+
+
+def from_json_string(my_str):
+    """Return the Python object represented by a JSON string."""
+    return jon.loads(my_str)
+
