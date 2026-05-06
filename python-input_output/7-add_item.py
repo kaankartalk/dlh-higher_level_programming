f5c9a966bcbe52cb6083b3389bf01ed6c0b36c45
Kaan Kartal Kuyucu <kkaankartal@gmail.com>
2026-05-06 16:57:08 +0000

    7-add

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-Script that adds command line arguments to a list
-and saves them to add_item.json.
+Script adds command line.
 """
 
 import sys
@@ -9,3 +8,12 @@ from 5-save_to_json_file import save_to_json_file
 from 6-load_from_json_file import load_from_json_file
 
 filename = "add_item.json"
+
+try:
+    items = load_from_json_file(filename)
+except Exception:
+    items = []
+
+iems.extend(sys.argv[1:])
+
+save_to_json_file(items, filename)
