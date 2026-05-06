f4175761f8b323303ac5c34617b36fc38058b6cf
Kaan Kartal Kuyucu <kkaankartal@gmail.com>
2026-05-06 17:17:58 +0000

    7-add

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """
-saves them to add_item.json.
+Adds all command line arguments to a list
+and saves them in add_item.json.
 """
 
 import sys
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
