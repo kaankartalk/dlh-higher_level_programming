d8f9fb7c7d35cd35ce5a6642d249d733c1002834
Kaan Kartal Kuyucu <kkaankartal@gmail.com>
2026-05-06 18:10:07 +0000

    10-student

---
+++ python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Serialization and filtering.
+"""
+
+
+class Student:
+    """Defines first name, last name, and age."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize the student with first name, last name, and age."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Return all attributes.
+        """
+        if isinstance(attrs, list) and all(type(a) is str for a in attrs):
+            return {key: self.__dict__[key] for key in attrs if key in self.__dict__}
+        eturn self.__dict__
